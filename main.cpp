38a73fb5cfb09912082a2b44620c643d264b4360
Narek <narek77-00@mail.ru>
2025-09-11 13:03:22 +0300

    Объединили клиент и сервер и починили пути

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 #include <QQmlContext>
 #include <QDir>
 #include <QDebug>
+#include "tcpclienthandler.h"
+#include "tcpserverhandler.h"
+
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    qmlRegisterType<TcpServerHandler>("FileServer", 1, 0, "TcpServerHandler");
+    qmlRegisterType<TcpClientHandler>("FileClient", 1, 0, "TcpClientHandler");
 
     QQmlApplicationEngine engine;
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
